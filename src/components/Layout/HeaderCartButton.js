13007eb20e5dcd1440390befd7fbcdbc80b85b0f
Colta Cristi <colta.cristi93@gmail.com>
2022-05-18 10:12:40 +0300

    Add cart context & provider

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import cartContext from '../../store/cart-context';
+import { useContext } from 'react';
 
 const HeaderCartButton = props => {
+  const cartCtx = useContext(cartContext);
+
+  const numberOfItems = cartCtx.items.reduce(
+    (acc, item) => acc + item.amount,
+    0,
+  );
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span> Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   );
 };
---
+++ src/store/cartProvider.js
@@ -0,0 +1,24 @@
+import CartContext from './cart-context';
+
+const cartProvider = props => {
+  const addItemHandler = item => {
+    //
+  };
+
+  const removeItemHandler = id => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemHandler,
+    removeItem: removeItemHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default cartProvider;
